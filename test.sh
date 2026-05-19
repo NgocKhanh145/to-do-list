a401a42970bdbcd17f5d7cff56f1bc89bf896914
Cao Giang Huy <caogianghuy1305@gmail.com>
2026-05-19 23:32:01 +0700

    123

--- test.sh
+++ test.sh
@@ -1,14 +1,10 @@
 $ git log --oneline
-# b4b040d (HEAD -> feat/backend, origin/feat/backend) frontend: thêm file js để gọi api
+# 48c6d63 (HEAD -> feat/backend, origin/feat/backend) Revert "frontend: thêm file js để gọi api"
+# 52622bd demo revert
+# b4b040d frontend: thêm file js để gọi api
 # 14bfa86 them sua
 # 0da4db9 thêm
 # 3274cdf Merge branch 'main' of https://github.com/NgocKhanh145/to-do-list into feat/backend
 # 0fc2241 xóa BE
-# 52e1725 (origin/feat/to-do-list-ui) feat: create initial to-do list frontend interface with styling and task management structure
-# 0056de5 feat: enhance UI for To Do List with new layout and styles
-# 07fbefb set up
-# 31ac189 feat: set up backend server
-# e75a9a6 them giao dien
-# 53c123d feat: setup express and gitignore
-# b33741a Setup project
 
+$ git reset 52622bd
